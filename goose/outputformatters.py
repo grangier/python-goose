de5ce5a3cb8942e49c617253d7f6aac4d654935f
Xavier Grangier <grangier@gmail.com>
2013-03-22 20:01:56 +0100

    add sup tag to replaceTagsWithText methode

--- goose/outputformatters.py
+++ goose/outputformatters.py
@@ -91,7 +91,7 @@ class OutputFormatter(object):
         with whatever text is inside them
         code : http://lxml.de/api/lxml.etree-module.html#strip_tags
         """
-        Parser.stripTags(self.getTopNode(), 'b', 'strong', 'i', 'br')
+        Parser.stripTags(self.getTopNode(), 'b', 'strong', 'i', 'br', 'sup')
 
     def removeParagraphsWithFewWords(self, article):
         """\
