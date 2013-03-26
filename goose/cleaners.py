f1762ef436b1d83b28fc6ec973581b086b905fcd
Xavier Grangier <grangier@gmail.com>
2013-03-26 08:53:27 +0100

    Cleaner class camelless methodes name

--- goose/cleaners.py
+++ goose/cleaners.py
@@ -28,7 +28,7 @@ class DocumentCleaner(object):
 
     def __init__(self):
 
-        self.regExRemoveNodes = (
+        self.remove_nodes_re = (
         "^side$|combx|retweet|mediaarticlerelated|menucontainer|navbar"
         "|comment|PopularQuestions|contact|foot|footer|Footer|footnote"
         "|cnn_strycaptiontxt|links|meta$|scroll|shoutbox|sponsor"
@@ -40,41 +40,41 @@ class DocumentCleaner(object):
         "|konafilter|KonaFilter|breadcrumbs|^fn$|wp-caption-text"
         "|source|legende|ajoutVideo|timestamp"
         )
-        self.regexpNS = "http://exslt.org/regular-expressions"
-        self.queryNaughtyIDs = "//*[re:test(@id, '%s', 'i')]" % self.regExRemoveNodes
-        self.queryNaughtyClasses = "//*[re:test(@class, '%s', 'i')]" % self.regExRemoveNodes
-        self.queryNaughtyNames = "//*[re:test(@name, '%s', 'i')]" % self.regExRemoveNodes
-        self.divToPElementsPattern = r"<(a|blockquote|dl|div|img|ol|p|pre|table|ul)"
-        self.captionPattern = "^caption$"
-        self.googlePattern = " google "
-        self.entriesPattern = "^[^entry-]more.*$"
-        self.facebookPattern = "[^-]facebook"
-        self.facebookBroadcastingPattern = "facebook-broadcasting"
-        self.twitterPattern = "[^-]twitter"
-        self.tabsAndNewLinesReplcesments = ReplaceSequence()\
+        self.regexp_namespace = "http://exslt.org/regular-expressions"
+        self.nauthy_ids_re = "//*[re:test(@id, '%s', 'i')]" % self.remove_nodes_re
+        self.nauthy_classes_re = "//*[re:test(@class, '%s', 'i')]" % self.remove_nodes_re
+        self.nauthy_names_re = "//*[re:test(@name, '%s', 'i')]" % self.remove_nodes_re
+        self.div_to_p_re = r"<(a|blockquote|dl|div|img|ol|p|pre|table|ul)"
+        self.caption_re = "^caption$"
+        self.google_re = " google "
+        self.entries_re = "^[^entry-]more.*$"
+        self.facebook_re = "[^-]facebook"
+        self.facebook_braodcasting_re = "facebook-broadcasting"
+        self.twitter_re = "[^-]twitter"
+        self.tablines_replacements = ReplaceSequence()\
                                             .create("\n", "\n\n")\
                                             .append("\t")\
                                             .append("^\\s+$")
 
     def clean(self, article):
 
-        docToClean = article.doc
-        docToClean = self.cleanEmTags(docToClean)
-        docToClean = self.removeDropCaps(docToClean)
-        docToClean = self.removeScriptsAndStyles(docToClean)
-        docToClean = self.cleanBadTags(docToClean)
-        docToClean = self.removeNodesViaRegEx(docToClean, self.captionPattern)
-        docToClean = self.removeNodesViaRegEx(docToClean, self.googlePattern)
-        docToClean = self.removeNodesViaRegEx(docToClean, self.entriesPattern)
-        docToClean = self.removeNodesViaRegEx(docToClean, self.facebookPattern)
-        docToClean = self.removeNodesViaRegEx(docToClean, self.facebookBroadcastingPattern)
-        docToClean = self.removeNodesViaRegEx(docToClean, self.twitterPattern)
-        docToClean = self.cleanUpSpanTagsInParagraphs(docToClean)
-        docToClean = self.convertDivsToParagraphs(docToClean, 'div')
-        docToClean = self.convertDivsToParagraphs(docToClean, 'span')
-        return docToClean
+        doc_to_clean = article.doc
+        doc_to_clean = self.clean_em_tags(doc_to_clean)
+        doc_to_clean = self.remove_drop_caps(doc_to_clean)
+        doc_to_clean = self.remove_scripts_styles(doc_to_clean)
+        doc_to_clean = self.clean_bad_tags(doc_to_clean)
+        doc_to_clean = self.remove_nodes_regex(doc_to_clean, self.caption_re)
+        doc_to_clean = self.remove_nodes_regex(doc_to_clean, self.google_re)
+        doc_to_clean = self.remove_nodes_regex(doc_to_clean, self.entries_re)
+        doc_to_clean = self.remove_nodes_regex(doc_to_clean, self.facebook_re)
+        doc_to_clean = self.remove_nodes_regex(doc_to_clean, self.facebook_braodcasting_re)
+        doc_to_clean = self.remove_nodes_regex(doc_to_clean, self.twitter_re)
+        doc_to_clean = self.clean_para_spans(doc_to_clean)
+        doc_to_clean = self.div_to_para(doc_to_clean, 'div')
+        doc_to_clean = self.div_to_para(doc_to_clean, 'span')
+        return doc_to_clean
 
-    def cleanEmTags(self, doc):
+    def clean_em_tags(self, doc):
         ems = Parser.getElementsByTag(doc, tag='em')
         for node in ems:
             images = Parser.getElementsByTag(node, tag='img')
@@ -82,14 +82,14 @@ class DocumentCleaner(object):
                 node.drop_tag()
         return doc
 
-    def removeDropCaps(self, doc):
+    def remove_drop_caps(self, doc):
         items = doc.cssselect("span[class~=dropcap], span[class~=drop_cap]")
         for item in items:
             item.drop_tag()
 
         return doc
 
-    def removeScriptsAndStyles(self, doc):
+    def remove_scripts_styles(self, doc):
         # remove scripts
         scripts = Parser.getElementsByTag(doc, tag='script')
         for item in scripts:
@@ -107,46 +107,46 @@ class DocumentCleaner(object):
 
         return doc
 
-    def cleanBadTags(self, doc):
+    def clean_bad_tags(self, doc):
 
         # ids
-        naughtyList = doc.xpath(self.queryNaughtyIDs,
-                                        namespaces={'re': self.regexpNS})
+        naughtyList = doc.xpath(self.nauthy_ids_re,
+                                        namespaces={'re': self.regexp_namespace})
         for node in naughtyList:
             Parser.remove(node)
 
         # class
-        naughtyClasses = doc.xpath(self.queryNaughtyClasses,
-                                        namespaces={'re': self.regexpNS})
+        naughtyClasses = doc.xpath(self.nauthy_classes_re,
+                                        namespaces={'re': self.regexp_namespace})
         for node in naughtyClasses:
             Parser.remove(node)
 
         # name
-        naughtyNames = doc.xpath(self.queryNaughtyNames,
-                                        namespaces={'re': self.regexpNS})
+        naughtyNames = doc.xpath(self.nauthy_names_re,
+                                        namespaces={'re': self.regexp_namespace})
         for node in naughtyNames:
             Parser.remove(node)
 
         return doc
 
-    def removeNodesViaRegEx(self, doc, pattern):
+    def remove_nodes_regex(self, doc, pattern):
         for selector in ['id', 'class']:
             reg = "//*[re:test(@%s, '%s', 'i')]" % (selector, pattern)
-            naughtyList = doc.xpath(reg, namespaces={'re': self.regexpNS})
+            naughtyList = doc.xpath(reg, namespaces={'re': self.regexp_namespace})
             for node in naughtyList:
                 Parser.remove(node)
         return doc
 
-    def cleanUpSpanTagsInParagraphs(self, doc):
+    def clean_para_spans(self, doc):
         spans = doc.cssselect('p > span')
         for item in spans:
             item.drop_tag()
         return doc
 
-    def getFlushedBuffer(self, replacementText, doc):
+    def get_flushed_buffer(self, replacementText, doc):
         return Parser.textToPara(replacementText)
 
-    def getReplacementNodes(self, doc, div):
+    def get_replacement_nodes(self, doc, div):
         replacementText = []
         nodesToReturn = []
         nodesToRemove = []
@@ -156,7 +156,7 @@ class DocumentCleaner(object):
             # node is a p
             # and already have some replacement text
             if Parser.getTag(kid) == 'p' and len(replacementText) > 0:
-                newNode = self.getFlushedBuffer(''.join(replacementText), doc)
+                newNode = self.get_flushed_buffer(''.join(replacementText), doc)
                 nodesToReturn.append(newNode)
                 replacementText = []
                 nodesToReturn.append(kid)
@@ -164,7 +164,7 @@ class DocumentCleaner(object):
             elif Parser.isTextNode(kid):
                 kidTextNode = kid
                 kidText = Parser.getText(kid)
-                replaceText = self.tabsAndNewLinesReplcesments.replaceAll(kidText)
+                replaceText = self.tablines_replacements.replaceAll(kidText)
                 if(len(replaceText)) > 1:
                     prevSibNode = Parser.previousSibling(kidTextNode)
                     while prevSibNode is not None \
@@ -198,7 +198,7 @@ class DocumentCleaner(object):
 
         # flush out anything still remaining
         if(len(replacementText) > 0):
-            newNode = self.getFlushedBuffer(''.join(replacementText), doc)
+            newNode = self.get_flushed_buffer(''.join(replacementText), doc)
             nodesToReturn.append(newNode)
             replacementText = []
 
@@ -207,10 +207,10 @@ class DocumentCleaner(object):
 
         return nodesToReturn
 
-    def replaceElementsWithPara(self, doc, div):
+    def replace_with_para(self, doc, div):
         Parser.replaceTag(div, 'p')
 
-    def convertDivsToParagraphs(self, doc, domType):
+    def div_to_para(self, doc, domType):
         badDivs = 0
         elseDivs = 0
         divs = Parser.getElementsByTag(doc, tag=domType)
@@ -219,10 +219,10 @@ class DocumentCleaner(object):
         for div in divs:
             items = Parser.getElementsByTags(div, tags)
             if div is not None and len(items) == 0:
-                self.replaceElementsWithPara(doc, div)
+                self.replace_with_para(doc, div)
                 badDivs += 1
             elif div is not None:
-                replaceNodes = self.getReplacementNodes(doc, div)
+                replaceNodes = self.get_replacement_nodes(doc, div)
                 div.clear()
 
                 for c, n in enumerate(replaceNodes):
