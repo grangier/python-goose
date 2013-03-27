01a2a833d659375a9363b3448e1ed39b7749bfe4
Xavier Grangier <grangier@gmail.com>
2013-03-27 08:19:25 +0100

    replace getHtml with get_html

--- tests/tests.py
+++ tests/tests.py
@@ -32,7 +32,7 @@ import pprint
 
 class TestParser(unittest.TestCase):
 
-    def getHtml(self, filename):
+    def get_html(self, filename):
         return FileHelper.loadResourceFile(filename)
 
     def test_childNodesWithText(self):
@@ -44,7 +44,7 @@ class TestParser(unittest.TestCase):
         p = Parser.getElementsByTag(doc, tag='p')[0]
 
     def test_replacetag(self):
-        html = self.getHtml('parser/test1.html')
+        html = self.get_html('parser/test1.html')
         doc = Parser.fromstring(html)
 
         # replace all p with div
@@ -180,7 +180,7 @@ class TestExtractions(unittest.TestCase):
     def setUp(self):
         self.articleReport = ["=======================::. ARTICLE REPORT .::======================\n"]
 
-    def getHtml(self, filename):
+    def get_html(self, filename):
         return FileHelper.loadResourceFile(filename)
 
     def getArticle(self, url, raw_html, language=None):
@@ -271,7 +271,7 @@ class TestExtractions(unittest.TestCase):
         pprint.pprint(self.articleReport)
 
     def test_cnn1(self):
-        html = self.getHtml('statichtml/cnn1.txt')
+        html = self.get_html('statichtml/cnn1.txt')
         url = "http://www.cnn.com/2010/POLITICS/08/13/democrats.social.security/index.html"
         title = "Democrats to use Social Security against GOP this fall"
         content = "Washington (CNN) -- Democrats pledged "
@@ -280,7 +280,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_businessWeek1(self):
-        html = self.getHtml("statichtml/businessweek1.txt")
+        html = self.get_html("statichtml/businessweek1.txt")
         url = "http://www.businessweek.com/magazine/content/10_34/b4192066630779.htm"
         title = "Olivia Munn: Queen of the Uncool"
         content = "Six years ago, Olivia Munn arrived in Hollywood with fading ambitions of making it as a sports reporter and set about deploying"
@@ -289,7 +289,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_businessWeek2(self):
-        html = self.getHtml("statichtml/businessweek2.txt")
+        html = self.get_html("statichtml/businessweek2.txt")
         url = "http://www.businessweek.com/management/five-social-media-lessons-for-business-09202011.html"
         title = "Five Social Media Lessons for Business"
         content = "At Home Depot, we first realized we needed to have a real conversation with"
@@ -298,7 +298,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_businessWeek3(self):
-        html = self.getHtml("statichtml/businessweek3.txt")
+        html = self.get_html("statichtml/businessweek3.txt")
         url = "http://www.businessweek.com/technology/here-comes-apples-real-tv-09132011.html"
         content = "Get ready, America, because by Christmas 2012 you will have an Apple TV in your living room"
         article = self.getArticle(url, html)
@@ -306,7 +306,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_cbslocal(self):
-        html = self.getHtml("statichtml/cbslocal1.txt")
+        html = self.get_html("statichtml/cbslocal1.txt")
         url = "http://newyork.cbslocal.com/2012/06/08/bc-morning-show-american-hero-kelly-malloy/"
         content = "Boomer & Craig were thrilled to welcome an American Hero into the Allstate Studio, as Kelly"
         article = self.getArticle(url, html)
@@ -314,7 +314,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_elmondo1(self):
-        html = self.getHtml("statichtml/elmondo1.txt")
+        html = self.get_html("statichtml/elmondo1.txt")
         url = "http://www.elmundo.es/elmundo/2012/10/28/espana/1351388909.html"
         content = "Importante golpe a la banda terrorista ETA en Francia."
         article = self.getArticle(url, html, language='es')
@@ -322,7 +322,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_elpais(self):
-        html = self.getHtml("statichtml/elpais.txt")
+        html = self.get_html("statichtml/elpais.txt")
         url = "http://www.sociedad.elpais.com/sociedad/2012/10/27/actualidad/1351332873_157836.html"
         content = "Los recortes pasan factura a los pacientes."
         article = self.getArticle(url, html)
@@ -330,7 +330,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_liberation(self):
-        html = self.getHtml("statichtml/liberation.txt")
+        html = self.get_html("statichtml/liberation.txt")
         url = "http://www.liberation.fr/politiques/2012/10/27/ayrault-assume-et-revendique-sa-methode_856451"
         content = "A Toulouse, Jean-Marc Ayrault aura fait deux rappels sur"
         article = self.getArticle(url, html)
@@ -341,7 +341,7 @@ class TestExtractions(unittest.TestCase):
     # makes lxml crash
     # python: double free or corruption
     def test_techcrunch1(self):
-        html = self.getHtml("statichtml/techcrunch1.txt")
+        html = self.get_html("statichtml/techcrunch1.txt")
         url = "http://techcrunch.com/2011/08/13/2005-zuckerberg-didnt-want-to-take-over-the-world/"
         content = "The Huffington Post has come across this fascinating five-minute interview"
         title = u"2005 Zuckerberg Didn’t Want To Take Over The World"
@@ -350,7 +350,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_foxNews(self):
-        html = self.getHtml("statichtml/foxnews1.txt")
+        html = self.get_html("statichtml/foxnews1.txt")
         url = "http://www.foxnews.com/politics/2010/08/14/russias-nuclear-help-iran-stirs-questions-improved-relations/"
         content = "Russia's announcement that it will help Iran get nuclear fuel is raising questions"
         article = self.getArticle(url, html)
@@ -358,7 +358,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_aolNews(self):
-        html = self.getHtml("statichtml/aol1.txt")
+        html = self.get_html("statichtml/aol1.txt")
         url = "http://www.aolnews.com/nation/article/the-few-the-proud-the-marines-getting-a-makeover/19592478"
         content = "WASHINGTON (Aug. 13) -- Declaring \"the maritime soul of the Marine Corps\" is"
         article = self.getArticle(url, html)
@@ -366,7 +366,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_huffingtonPost2(self):
-        html = self.getHtml("statichtml/huffpo2.txt")
+        html = self.get_html("statichtml/huffpo2.txt")
         url = "http://www.huffingtonpost.com/2011/10/06/alabama-workers-immigration-law_n_997793.html"
         content = "MONTGOMERY, Ala. -- Alabama's strict new immigration law may be backfiring."
         article = self.getArticle(url, html)
@@ -374,7 +374,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_testHuffingtonPost(self):
-        html = self.getHtml("statichtml/huffpo1.txt")
+        html = self.get_html("statichtml/huffpo1.txt")
         url = "http://www.huffingtonpost.com/2010/08/13/federal-reserve-pursuing_n_681540.html"
         title = "Federal Reserve's Low Rate Policy Is A 'Dangerous Gamble,' Says Top Central Bank Official"
         content = "A top regional Federal Reserve official sharply criticized Friday"
@@ -385,7 +385,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_espn(self):
-        html = self.getHtml("statichtml/espn1.txt")
+        html = self.get_html("statichtml/espn1.txt")
         url = "http://sports.espn.go.com/espn/commentary/news/story?id=5461430"
         content = "If you believe what college football coaches have said about sports"
         article = self.getArticle(url, html)
@@ -393,7 +393,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_engadget(self):
-        html = self.getHtml("statichtml/engadget1.txt")
+        html = self.get_html("statichtml/engadget1.txt")
         url = "http://www.engadget.com/2010/08/18/verizon-fios-set-top-boxes-getting-a-new-hd-guide-external-stor/"
         content = "Streaming and downloading TV content to mobiles is nice"
         article = self.getArticle(url, html)
@@ -401,9 +401,9 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_msn1(self):
-        html = self.getHtml("statichtml/msn1.txt")
+        html = self.get_html("statichtml/msn1.txt")
         url = "http://lifestyle.msn.com/your-life/your-money-today/article.aspx?cp-documentid=31244150"
-        expected = self.getHtml("statichtml/msn1_result.txt")
+        expected = self.get_html("statichtml/msn1_result.txt")
         article = self.getArticle(url, html)
         self.runArticleAssertions(article=article, expectedStart=expected)
         self.printReport()
@@ -412,15 +412,15 @@ class TestExtractions(unittest.TestCase):
     # # FAIL CHECK
     # # UNICODE
     # def test_guardian1(self):
-    #     html = self.getHtml("statichtml/guardian1.txt")
+    #     html = self.get_html("statichtml/guardian1.txt")
     #     url = "http://www.guardian.co.uk/film/2011/nov/18/kristen-wiig-bridesmaids"
-    #     expected = self.getHtml("statichtml/guardian1_result.txt")
+    #     expected = self.get_html("statichtml/guardian1_result.txt")
     #     article = self.getArticle(url, html)
     #     self.runArticleAssertions(article=article, expectedStart=expected)
     #     self.printReport()
 
     def test_time(self):
-        html = self.getHtml("statichtml/time1.txt")
+        html = self.get_html("statichtml/time1.txt")
         url = "http://www.time.com/time/health/article/0,8599,2011497,00.html"
         title = "Invisible Oil from BP Spill May Threaten Gulf Aquatic Life"
         content = "This month, the federal government released"
@@ -429,7 +429,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_time2(self):
-        html = self.getHtml("statichtml/time2.txt")
+        html = self.get_html("statichtml/time2.txt")
         url = "http://newsfeed.time.com/2011/08/24/washington-monument-closes-to-repair-earthquake-induced-crack/"
         content = "Despite what the jeers of jaded Californians might suggest"
         article = self.getArticle(url, html)
@@ -437,7 +437,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_cnet(self):
-        html = self.getHtml("statichtml/cnet1.txt")
+        html = self.get_html("statichtml/cnet1.txt")
         url = "http://news.cnet.com/8301-30686_3-20014053-266.html?tag=topStories1"
         content = "NEW YORK--Verizon Communications is prepping a new"
         article = self.getArticle(url, html)
@@ -445,7 +445,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_yahoo(self):
-        html = self.getHtml("statichtml/yahoo1.txt")
+        html = self.get_html("statichtml/yahoo1.txt")
         url = "http://news.yahoo.com/apple-says-steve-jobs-resigning-ceo-224628633.html"
         content = u"SAN FRANCISCO (AP) — Steve Jobs, the mind behind the iPhone"
         article = self.getArticle(url, html)
@@ -453,7 +453,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_politico(self):
-        html = self.getHtml("statichtml/politico1.txt")
+        html = self.get_html("statichtml/politico1.txt")
         url = "http://www.politico.com/news/stories/1010/43352.html"
         content = "If the newest Census Bureau estimates stay close to form"
         article = self.getArticle(url, html)
@@ -461,7 +461,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_businessinsider1(self):
-        html = self.getHtml("statichtml/businessinsider1.txt")
+        html = self.get_html("statichtml/businessinsider1.txt")
         url = "http://articles.businessinsider.com/2011-09-21/markets/30183619_1_parliament-vote-greece-civil-servants"
         content = "As everyone in the world was transfixed on the Fed"
         article = self.getArticle(url, html)
@@ -469,7 +469,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_businessinsider2(self):
-        html = self.getHtml("statichtml/businessinsider2.txt")
+        html = self.get_html("statichtml/businessinsider2.txt")
         url = "http://www.businessinsider.com/goldman-on-the-fed-announcement-2011-9"
         content = "From Goldman on the FOMC operation twist announcement"
         article = self.getArticle(url, html)
@@ -480,7 +480,7 @@ class TestExtractions(unittest.TestCase):
     # FAIL
     # TEXT APPEND
     def test_cnbc1(self):
-        html = self.getHtml("statichtml/cnbc1.txt")
+        html = self.get_html("statichtml/cnbc1.txt")
         url = "http://www.cnbc.com/id/44613978"
         content = "Some traders found Wednesday's Fed statement to be a bit gloomier than expected."
         article = self.getArticle(url, html)
@@ -488,15 +488,15 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_issue24(self):
-        html = self.getHtml("statichtml/issue_24.txt")
+        html = self.get_html("statichtml/issue_24.txt")
         url = "http://danielspicar.github.com/goose-bug.html"
-        expected = self.getHtml("statichtml/issue_24_result.txt")
+        expected = self.get_html("statichtml/issue_24_result.txt")
         article = self.getArticle(url, html)
         self.runArticleAssertions(article=article, expectedStart=expected)
         self.printReport()
 
     def test_issue25(self):
-        html = self.getHtml("statichtml/issue_25.txt")
+        html = self.get_html("statichtml/issue_25.txt")
         url = "http://www.accountancyage.com/aa/analysis/2111729/institutes-ifrs-bang"
         expected = "UK INSTITUTES have thrown their weight behind rapid adoption of international financial reporting standards in the US."
         article = self.getArticle(url, html)
@@ -506,7 +506,7 @@ class TestExtractions(unittest.TestCase):
     #########################################
     # FAIL
     def test_issue28(self):
-        html = self.getHtml("statichtml/issue_28.txt")
+        html = self.get_html("statichtml/issue_28.txt")
         url = "http://www.telegraph.co.uk/foodanddrink/foodanddrinknews/8808120/Worlds-hottest-chilli-contest-leaves-two-in-hospital.html"
         expected = "Emergency services were called to Kismot Restaurant's curry-eating challenge,"
         article = self.getArticle(url, html)
@@ -514,7 +514,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_issue32(self):
-        html = self.getHtml("statichtml/issue_32.txt")
+        html = self.get_html("statichtml/issue_32.txt")
         url = "http://www.tulsaworld.com/site/articlepath.aspx?articleid=20111118_61_A16_Opposi344152&rss_lnk=7"
         expected = "Opposition to a proposal to remove certain personal data"
         article = self.getArticle(url, html)
@@ -522,7 +522,7 @@ class TestExtractions(unittest.TestCase):
         self.printReport()
 
     def test_issue4(self):
-        html = self.getHtml("statichtml/issue_4.txt")
+        html = self.get_html("statichtml/issue_4.txt")
         url = "http://www.slate.fr/story/64063/tapie-mougeotte-la-provence"
         expected = u"Exercice: apr\xe8s avoir attentivement lu cette br\xe8ve parue dans L'Express, vous expliquerez en quoi elle r\xe9sume une certaine id\xe9e de la France.\n\n\xabBernar"
         article = self.getArticle(url, html)
