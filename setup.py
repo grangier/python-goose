c1a25c556bf77322e7e05eceb669bf8c7e6d25dc
Xavier Grangier <grangier@gmail.com>
2013-03-27 08:57:46 +0100

    bump to v0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='goose',
-    version='0.0.1',
+    version='0.2',
     description="Html Content / Article Extractor",
     long_description="",
     keywords='',
