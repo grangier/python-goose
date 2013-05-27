8c295c1d66b2a27b5ee024373f31cf8a3c59fb2b
Xavier Grangier <grangier@gmail.com>
2013-05-27 08:45:20 +0200

    Goose requires bs3 instead of bs4

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(name='goose',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['PIL', 'lxml', 'cssselect', 'jieba', 'beautifulsoup4'],
+    install_requires=['PIL', 'lxml', 'cssselect', 'jieba', 'beautifulsoup'],
     test_suite="tests"
 )
