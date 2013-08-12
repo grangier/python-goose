a8e242c100cdf9616ef9c4b896fec704344f26f4
Xavier Grangier <grangier@gmail.com>
2013-08-12 23:28:12 +0200

    basic video extraction

--- goose/videos/videos.py
+++ goose/videos/videos.py
@@ -21,8 +21,28 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-
 class Video(object):
+    """\
+    Video object
+    """
 
-    def __init_(self):
-        pass
+    def __init__(self):
+
+        # type of embed
+        # embed, object, iframe
+        self.embed_type = None
+
+        # video provider name
+        self.provider = None
+
+        # width
+        self.width = None
+
+        # height
+        self.height = None
+
+        # embed code
+        self.embed_code = None
+
+        # src
+        self.src = None
