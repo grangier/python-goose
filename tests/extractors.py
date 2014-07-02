1563b91e4acb170cd1a0765ea22e98943cf16df0
Jeffrey Nappi <jeff@iacquire.com>
2014-07-02 15:04:05 -0700

    Updated bad classes regex
    * Sites with *links* in a class previously failed. This should likely be an exact match style (^links$)
    * Test included.

--- tests/extractors.py
+++ tests/extractors.py
@@ -350,6 +350,11 @@ class TestExtractions(TestExtractionBase):
         fields = ['cleaned_text']
         self.runArticleAssertions(article=article, fields=fields)
 
+    def test_okaymarketing(self):
+        article = self.getArticle()
+        fields = ['cleaned_text']
+        self.runArticleAssertions(article=article, fields=fields)
+
 
 class TestExtractWithUrl(TestExtractionBase):
 
