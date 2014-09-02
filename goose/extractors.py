b8c1fa15f8d496a8a84b1cb35c5a66732e434fbc
Joan <joanfihu@gmail.com>
2014-09-02 15:15:26 +0100

    looking author at  meta tags first

--- goose/extractors.py
+++ goose/extractors.py
@@ -64,7 +64,14 @@ class ContentExtractor(object):
         author = ''
         doc = self.article.doc
 
+        #Look at meta tags first
+        author = self.get_meta_author()
+
+        if (author != None) & (author != ''):
+            return author
+
         author_element = self.parser.getElementsByTag(doc, tag='a' , attr = 'href')
+        
         # no author found
         if author_element is None or len(author_element) == 0:
             return author
@@ -72,9 +79,10 @@ class ContentExtractor(object):
         for a in author_element:
             a_href = self.parser.getAttribute(a,'href')
             if a_href is not None:
-                #find url that have author | people or users
+                #find url that has author, people or users
                 if re.search('.author.', a_href) is not None:
                     author = self.clean_author(a_href)
+                    break
                 elif re.search('.people.', a_href) is not None:
                     author = self.clean_author(a_href)
                 elif re.search('.users.', a_href) is not None:
@@ -212,6 +220,12 @@ class ContentExtractor(object):
         if the article has meta description set in the source, use that
         """
         return self.get_meta_content(self.article.doc, "meta[name=description]")
+
+    def get_meta_author(self):
+        """\
+        if the article has meta Author set in the source, use that
+        """
+        return self.get_meta_content(self.article.doc, "meta[name=Author]")
 
     def get_meta_keywords(self):
         """\
