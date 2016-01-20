c64b245a8a6267c900365cc26070344afad30959
Lol4to <sidorov@satissoft.ru>
2016-01-20 18:27:12 +0300

    Fix deprecated class warning

--- goose/outputformatters.py
+++ goose/outputformatters.py
@@ -20,7 +20,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import html
 from six.moves.html_parser import HTMLParser
+import sys
 
 from goose.text import innerTrim
 
@@ -67,12 +69,14 @@ class OutputFormatter(object):
         self.remove_fewwords_paragraphs()
         return self.convert_to_text()
 
+    _text_parser = HTMLParser() if sys.version_info[0] == 2 else html
+
     def convert_to_text(self):
         txts = []
         for node in list(self.get_top_node()):
             txt = self.parser.getText(node)
             if txt:
-                txt = HTMLParser().unescape(txt)
+                txt = self._text_parser.unescape(txt)
                 txt_lis = innerTrim(txt).split(r'\n')
                 txts.extend(txt_lis)
         return '\n\n'.join(txts)
