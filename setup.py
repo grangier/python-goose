abd427bf2e1a3405fefeac6532b5f13563ed450b
Lol4to <sidorov@satissoft.ru>
2016-01-21 11:56:01 +0300

    Adjust classifiers
    Python 3.4, Python 3.5 added

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Topic :: Internet',
     'Topic :: Utilities',
     'Topic :: Software Development :: Libraries :: Python Modules']
